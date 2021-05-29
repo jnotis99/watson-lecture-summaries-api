73042bc8387c6368e6c5de866da6755782e74ae9
Joseph Notis <joseph.a.notis.21@dartmouth.edu>
2021-05-29 10:20:08 -0400

    basic routing skeleton

---
+++ src/routers/index.js
@@ -0,0 +1,3 @@
+import lectureRouter from './lectureRouter';
+
+export default { lectureRouter };
--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+import mongoose from 'mongoose';
+import routes from './routers/index';
+
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost/watsonLectures';
+
+mongoose.connect(mongoURI).then(() => {
+  console.log('connected to database:', mongoURI);
+}).catch((err) => {
+  console.log('error: could not connect to db:', err);
+});
 
 // initialize
 const app = express();
@@ -23,8 +33,10 @@ app.get('/', (req, res) => {
   res.send('hi');
 });
 
+app.use('/api/lectures', routes.lectureRouter);
+
 // LAUNCH SERVER
-// --------------
+// ----------------
 const port = process.env.PORT || 9090;
 app.listen(port);
 
